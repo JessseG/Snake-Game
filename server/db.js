9b2cc8f825cb0c88b267cf23e0aac2f024ef0d48
JessseG <61717513+JessseG@users.noreply.github.com>
2020-10-02 01:39:01 -0400

    v2

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "postgres",
-  password: "Fc8b47368cadc7e1",
+  password: "samplepassword",
   host: "localhost",
   port: 5432,
   database: "snake2",
